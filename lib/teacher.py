dc492ab75bced85efd6be7cc07e59136db5c47c5
Dakota Vikdal <dakota.james19@hotmail.com>
2023-03-20 11:54:25 -0400

    inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -3,8 +3,36 @@
 from user import User
 
 import random
+# import ipdb
+
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast"
+        ]
+
     def teach(self):
-        pass
+        index = random.randint(0, len(self.knowledge) -1)
+        return self.knowledge[index]
+        # self.knowledge = knowledge
+
+    # def get_info(self):
+        # return self._knowledge
+
+    # def set_info(self, info):
+        # if(info == knowledge):
+            # self._info = info
+        # else:
+            # print("not knowledge")
+
+books = Teacher("first_name", "last_name")
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
-
+#import ipdb
 class User:
-    pass
+    
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+dakota = User("Dakota", "Vikdal")
+#ipdb.set_trace()
